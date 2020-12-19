423c7cf677888bae498d9f3c9a0d03892712b30b
Habib <57207750+habibrayhan007@users.noreply.github.com>
2020-12-19 19:27:45 +0600

    Admin and User page design added

---
+++ admin.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin</title>
+	
+	<link rel="stylesheet" type="text/css" href="css/style.css">	
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body class="box">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-lg-5 bg-light mt-5 px-0">
+				<h3 class="text-center  p-4">Welcome Admin </h3>	
+			</div>
+			
+		</div>
+	</div>
+
+</body>
+</html>
---
+++ user.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>User</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">	
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body class="box">
+<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-lg-5 bg-light mt-5 px-0">
+				<h3 class="text-center  p-4">Welcome User</h3>
+
+				
+				
+			</div>
+			
+		</div>
+</body>
+</html>
